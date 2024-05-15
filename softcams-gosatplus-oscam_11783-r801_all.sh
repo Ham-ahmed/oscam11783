22a80d5fb9b68cc9207ce8b38abfc73668147c4a
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-05-16 02:35:30 +0300

    Update softcams-gosatplus-oscam_11783-r801_all.sh

--- softcams-gosatplus-oscam_11783-r801_all.sh
+++ softcams-gosatplus-oscam_11783-r801_all.sh
@@ -2,11 +2,11 @@
 #!/bin/sh
 #
 
-wget -O /var/volatile/tmp/enigma2-plugin-softcams-gosatplus-oscam_11783-emu-r801_all.ipk "https://raw.githubusercontent.com/Ham-ahmed/oscam11783/main/enigma2-plugin-softcams-gosatplus-oscam_11783-emu-r801_all.ipk"
+wget -O /var/volatile/tmp/main/enigma2-plugin-softcams-oscam_11783-emu-r801_all.ipk "https://raw.githubusercontent.com/Ham-ahmed/oscam11783/main/enigma2-plugin-softcams-oscam_11783-emu-r801_all.ipk"
 wait
 opkg install --force-overwrite /tmp/*.ipk
 wait
-rm -f /var/volatile/tmp/enigma2-plugin-softcams-gosatplus-oscam_11783-emu-r801_all.ipk
+rm -f /var/volatile/tmp/main/enigma2-plugin-softcams-oscam_11783-emu-r801_all.ipk
 wait
 sleep 2;
 exit 0
